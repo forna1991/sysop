dba1e32c1c685f008ad8d7a7dddfd6ab632c290b
Zambo <lucazamboni92@gmail.com>
2013-05-23 17:24:09 +0200

    Preparato un po di roba

--- equals.d/equals.c
+++ equals.d/equals.c
@@ -1,32 +1,37 @@
-/* 
- * File:   equals.c
- * Author: forna
- *
- * Created on 23 maggio 2013, 10.55
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <time.h>
 #include <sys/types.h>
 
+FILE *logger;
 
+int openLog();
+char* getTime();
 int equals(char *, char *);
 
-int main() {
+int main(int argc, char **argv) {
+	openLog();
 
+	// stampa il comando inserito nel file di log
+	fprintf(logger, "(II) Comando inserito : "); 
+	int i;
+	for (i = 0; i < argc; i += 1) {
+		fprintf(logger, "%s ", argv[i]);
+	}
+	fprintf(logger, "\n");
 }
 
 int equals(char * patha, char * pathb) {
 	//controllo sul pathing
 	//se e' una cartella rilanciare su tutti gli elementi
 	//se e' un file vedere se nell'altro path c'e' un file e nel caso confrontarli tra loro
-	int retval = 1; //TRUE
+	/*int retval = 1; //TRUE
 	char * tmpa, tmpb;
 	struct stat a,b;
-	tmpa = malloc(snprintf(NULL, 0, "%s",tmpa) + 1);
+	tmpa = malloc(256 * sizeof(char));
 	sprintf(tmpa, "%s",patha);
-	tmpb = malloc(snprintf(NULL, 0, "%s",tmpb) + 1);
+	tmpb = malloc(256 * sizeof(char));
 	sprintf(tmpb, "%s",pathb);
 
 	if( stat(tmpa,&a) == 0 && stat(tmpb,&b)) {	
@@ -44,5 +49,35 @@ int equals(char * patha, char * pathb) {
 				//lanciare ricorsivamente su tutti i sottofile delle due cartelle
 			}
 		}
-	}
+	}*/
+}
+
+//funzione che ritorna la data sottoforma di stringa
+char* getTime() {
+	time_t rawtime;
+  	struct tm * timeinfo;	
+	time ( &rawtime );
+	timeinfo = localtime ( &rawtime );
+	return asctime(timeinfo);
+}
+
+/*Funzione che serve per apre il file di log*/
+int openLog() {
+	int retval;
+
+	// controlla se c'è già un file di log creato
+    if (logger = fopen("./LogEquals", "r")) {
+        fclose(logger);
+        retval = 1;
+    } else
+    	retval = 0;
+    logger = fopen("./LogEquals", "a");
+
+    // se non esiste lo crea e ci scrive 2 righe di informazione
+    if (retval == 0) {
+    	fprintf(logger, "Markers: (!!) notice, (II) informational, (WW) warning, (EE) error\n");
+    	fprintf(logger, "(!!) Creazione log %s", getTime());
+    }
+   	fprintf(logger, "(II) Nuova esecuzione del programma al tempo: %s", getTime()); 
+    return retval;
 }
